c15eb82e61f416f6ed4108bff89d177d56092bc2
Liv <livkistrup@gmail.com>
2026-09-25 21:53:02 +0200

    try-catch

--- Program.cs
+++ Program.cs
@@ -3,17 +3,26 @@ using System;
 
 string worldPath = @"C:\MinecraftSyncTest\WorldA";
 
-if (Directory.Exists(worldPath) == true){
-    DateTime dt  = Directory.GetCreationTime(worldPath);
-    string[] files = Directory.GetFiles(worldPath);
-    Console.WriteLine("It exists");
-    Console.WriteLine(worldPath + "created at " + dt + " time");
-    foreach(string file in files){
-        string filename = Path.GetFileName(file);
-        Console.WriteLine(filename);
+string newPath = @"C:\MinecraftSyncTest\WorldB";
 
+if (Directory.Exists(worldPath) == true && Directory.Exists(newPath)){
+        DateTime dt  = Directory.GetCreationTime(worldPath);
+        string[] files = Directory.GetFiles(worldPath);
+        Console.WriteLine("It exists");
+        Console.WriteLine(worldPath + "created at " + dt + " time");
+        foreach(string file in files){
+            try{
+                string filename = Path.GetFileName(file);
+                string destination = Path.Combine(newPath, filename);
+                File.Copy(file, destination);
+                Console.WriteLine(filename);  
+            }
+      catch{
+          string filename = Path.GetFileName(file);
+          Console.WriteLine("This file had a copy issue: " + filename);
+      }      
     }
-
 } else {
     Directory.CreateDirectory(worldPath);
+    Directory.CreateDirectory(newPath);
 }
--- obj/Debug/net10.0/MC_load_synch.AssemblyInfo.cs
+++ obj/Debug/net10.0/MC_load_synch.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MC_load_synch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6082a25569fb4134d5604b4840d071bf4090c6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+477cd2e42c739f319814f02e68e2cc7f17ab6a59")]
 [assembly: System.Reflection.AssemblyProductAttribute("MC_load_synch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MC_load_synch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
