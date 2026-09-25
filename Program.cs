477cd2e42c739f319814f02e68e2cc7f17ab6a59
Liv <livkistrup@gmail.com>
2026-09-25 21:30:41 +0200

    test1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.IO;
+using System;
+
+string worldPath = @"C:\MinecraftSyncTest\WorldA";
+
+if (Directory.Exists(worldPath) == true){
+    DateTime dt  = Directory.GetCreationTime(worldPath);
+    string[] files = Directory.GetFiles(worldPath);
+    Console.WriteLine("It exists");
+    Console.WriteLine(worldPath + "created at " + dt + " time");
+    foreach(string file in files){
+        string filename = Path.GetFileName(file);
+        Console.WriteLine(filename);
+
+    }
+
+} else {
+    Directory.CreateDirectory(worldPath);
+}
