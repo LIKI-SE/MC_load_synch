5a5d5c47eb8fa2f7f3b1b70c901a8714fe84a79a
Liv <livkistrup@gmail.com>
2026-09-26 11:28:12 +0200

    Case and switch

--- Program.cs
+++ Program.cs
@@ -1,42 +1,70 @@
 ﻿using System.IO;
 using System;
 
+
+
 string worldPath = @"C:\MinecraftSyncTest\WorldA";
 
 string newPath = @"C:\MinecraftSyncTest\WorldB";
 
-if (Directory.Exists(worldPath) == true && Directory.Exists(newPath)){
-        DateTime dt  = Directory.GetCreationTime(worldPath);
-        string[] files = Directory.GetFiles(worldPath);
-        Console.WriteLine(worldPath + "created at " + dt + " time");
-        foreach(string file in files){
-            try{
-                string filename = Path.GetFileName(file);
-                string destination = Path.Combine(newPath, filename);
-                DateTime dt_org  = File.GetLastWriteTimeUtc(file);
-                DateTime dt_copy  = File.GetLastWriteTimeUtc(destination);
+Console.WriteLine("Hello, welcome to MC_load_synch");
 
-                if (File.Exists(destination) == false){
-                    File.Copy(file, destination);
-                    Console.WriteLine("File copied: " + filename);
-                    
-                } else if (dt_org != dt_copy) {
-                    Console.WriteLine("File out of synch: " + filename);
-                    File.Delete(destination);
-                    File.Copy(file, destination);
+Console.WriteLine("Press A for synch A->B");
+Console.WriteLine("Press B for synch direction");
+Console.WriteLine("Press C for automatic folder creation");
 
-                } else {
-                    Console.WriteLine("File skipped: " + filename);
-                }
+string Synch = Console.ReadLine();
+
+switch (Synch){
+    case "A":
+    if (Directory.Exists(worldPath) == true && Directory.Exists(newPath)){
+            DateTime dt  = Directory.GetCreationTime(worldPath);
+            string[] files = Directory.GetFiles(worldPath);
+            Console.WriteLine(worldPath + "created at " + dt + " time");
+            foreach(string file in files){
+                try{
+                    string filename = Path.GetFileName(file);
+                    string destination = Path.Combine(newPath, filename);
+                    DateTime dt_org  = File.GetLastWriteTimeUtc(file);
+                    DateTime dt_copy  = File.GetLastWriteTimeUtc(destination);
     
- 
-            }
-      catch{
-          string filename = Path.GetFileName(file);
-          Console.WriteLine("This file had a copy issue: " + filename);
-      }      
+                    if (File.Exists(destination) == false){
+                        File.Copy(file, destination);
+                        Console.WriteLine("File copied: " + filename);
+                        
+                    } else if (dt_org != dt_copy) {
+                        Console.WriteLine("File out of synch: " + filename);
+                        File.Delete(destination);
+                        File.Copy(file, destination);
+    
+                    } else {
+                        Console.WriteLine("File skipped: " + filename);
+                    }
+        
+     
+                }
+          catch{
+              string filename = Path.GetFileName(file);
+              Console.WriteLine("This file had a copy issue: " + filename);
+          }      
+        }
+    } else {
+        Directory.CreateDirectory(worldPath);
+        Directory.CreateDirectory(newPath);
     }
-} else {
-    Directory.CreateDirectory(worldPath);
-    Directory.CreateDirectory(newPath);
+    break;
+    case "B":
+    Console.WriteLine("What path do you source to be?");
+    worldPath = Console.ReadLine();
+    Console.WriteLine("New source path is: " + worldPath);
+    Console.WriteLine("What path do you destination to be?");
+    newPath = Console.ReadLine();
+    Console.WriteLine("New desitnation path is: " + newPath);
+    break;
+
+    case "C":
+    worldPath = @"C:\MinecraftSyncTest\WorldA";
+    newPath = @"C:\MinecraftSyncTest\WorldB";
+    break;
+
 }
--- obj/Debug/net10.0/MC_load_synch.AssemblyInfo.cs
+++ obj/Debug/net10.0/MC_load_synch.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MC_load_synch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c15eb82e61f416f6ed4108bff89d177d56092bc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+734211ecd7e77fafcb7be98c4e6e65b16c020075")]
 [assembly: System.Reflection.AssemblyProductAttribute("MC_load_synch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MC_load_synch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
